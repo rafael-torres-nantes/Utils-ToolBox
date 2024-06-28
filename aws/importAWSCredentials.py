4d4db53df4128bc1290b4fdef0be58f8830fdebf
Rafinha <58231791+rafael-torres-nantes@users.noreply.github.com>
2024-06-27 20:31:32 -0400

    feat: create service import aws credentials

---
+++ aws/importAWSCredentials.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+def aws_credentials():
+    # Carregar variáveis de ambiente do arquivo .env
+    load_dotenv()
+
+    # Acessa as variáveis definidas
+    ACESS_KEY = os.getenv('AWS_ACCESS_KEY_ID')
+    SECRET_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    SESSION_TOKEN = os.getenv('AWS_SECRET_SESSION_TOKEN')
+
+    return ACESS_KEY, SECRET_KEY, SESSION_TOKEN
