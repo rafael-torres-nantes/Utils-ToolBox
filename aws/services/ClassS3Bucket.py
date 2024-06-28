24e1795c041e6908bfedc9f25a76d84553623cd2
Rafinha <58231791+rafael-torres-nantes@users.noreply.github.com>
2024-06-27 20:55:59 -0400

    refactor: adding error handling

--- aws/services/ClassS3Bucket.py
+++ aws/services/ClassS3Bucket.py
@@ -11,11 +11,8 @@ class S3BucketClass:
         # Cria uma viaravel global bucket name
         self.bucket_name = bucket_name
 
-        # Importa as Credenciais da AWS
-        self.session = self.create_session()
-
         # Inicia o serviço S3 Bucket
-        self.s3_client = self.session.client('s3')
+        self.s3_client = boto3.client('s3')
          
     
     def create_session(self):
@@ -43,34 +40,26 @@ class S3BucketClass:
         :param bucket_name: Nome do bucket a ser criado
         :return: True se o bucket for criado com sucesso, caso contrário False
         """
-        # Importa o bucket name da Classe
-        bucket_name = self.bucket_name
-
-        try:
-            # Cria o bucket
-            self.s3_client.create_bucket(Bucket=bucket_name)
-            self.session.resource('s3').Bucket(bucket_name)
+        
+        try: # Verifica se o bucket já existe, caso não exista, cria o bucket
+            if self._bucket_exists(self.bucket_name):
+                print(f"Bucket {self.bucket_name} já existe.")
+                return True
+            self.s3_client.create_bucket(Bucket=self.bucket_name)
             
-        except ClientError as e:
-            logging.error(e)
+        except ClientError as e: # Caso ocorra um erro, imprime a mensagem de erro
+            logging.error(f"Erro ao criar o bucket: {e}")
             return False
         return True
 
-    def list_s3_bucket(self):
-        """
-        Lista os nomes dos buckets S3 existentes.
-
-        :return: Lista de nomes de buckets
-        """
-        # Recupera a lista de buckets existentes
-        list_buckets = self.s3_client.list_buckets()
-
-        # Exibe os nomes dos buckets
-        for bucket in list_buckets['Buckets']:
-            print(f'{bucket["Name"]}')
-
-        return list_buckets['Buckets']
-
+    # Método para verificar se o bucket já existe no S3
+    def _bucket_exists(self, bucket_name):
+        try: # Verifica se o bucket já existe no S3 e retorna True, caso exista
+            self.s3_client.head_bucket(Bucket=bucket_name)
+            return True
+        except ClientError: # Caso não exista, retorna False 
+            return False 
+            
     def upload_s3_bucket(self, upload_file, filename):
         """
         Faz o upload de um arquivo para um bucket S3.
@@ -80,20 +69,37 @@ class S3BucketClass:
         :param filename: Nome do objeto no S3. Se não especificado, o nome do arquivo será usado
         :return: True se o arquivo for enviado com sucesso, caso contrário False
         """
-        try:
-            # Faz o upload do arquivo
+        try: # Faz o upload do arquivo no S3 e retorna a URL do arquivo
             self.s3_client.upload_file(upload_file, self.bucket_name, filename)
+            file_url = f"https://{self.bucket_name}.s3.amazonaws.com/{filename}"
+            return file_url
 
         # Upload a object(The upload_fileobj method accepts a readable file-like object. The file object must be opened in binary mode, not text mode.)
         # with open("FILE_NAME", "rb") as f:
         #     s3_client.upload_fileobj(f, bucket_name, "OBJECT_NAME")
         
-        except ClientError as e:
-            logging.error(e)
-            return False
+        except ClientError as e: # Caso ocorra um erro, imprime a mensagem de erro
+            logging.error(f"Erro ao fazer upload do arquivo: {e}")
+            return None
+            
+    def list_s3_bucket(self):
+        """
+        Lista os nomes dos buckets S3 existentes.
 
+        :return: Lista de nomes de buckets
+        """
+        # Recupera a lista de buckets existentes
+        try:
+            response = self.s3_client.list_buckets()
+            
+            # Exibe os nomes dos buckets
+            print("S3 Buckets:", [bucket['Name'] for bucket in response['Buckets']])
+
+        except ClientError as e:
+            print(f"Error listing S3 buckets: {e}")
+            return False
         return True
-        
+
 
     def extract_file_s3_bucket(self, object_key):
         """
