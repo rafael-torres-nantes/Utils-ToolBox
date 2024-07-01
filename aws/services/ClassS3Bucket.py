4dbb0b0530a0b7b55c1b0be4bc7c43d71d05d6f1
Rafinha <58231791+rafael-torres-nantes@users.noreply.github.com>
2024-07-01 04:46:56 -0400

    feat: adding new functions on s3 class

--- aws/services/ClassS3Bucket.py
+++ aws/services/ClassS3Bucket.py
@@ -105,7 +105,52 @@ class S3BucketClass:
 
         return local_directory
 
-load_dotenv()
-S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
+    def upload_image_to_s3(image_name, bucket_name, object_name=None):
+    
+        """
+        Faz o upload de uma imagem para o bucket no S3
+    
+        image_name: imagem a ser enviada
+        object_name: nome do objeto no S3. Se none, image é usado
+        bucket_name: bucket para onde a imagem será enviada
+        return: True se o upload foi bem sucedido, False caso contrário
+        """
+    
+        if object_name is None:
+            object_name = image_name
+    
+        s3_client = boto3.client('s3')
+    
+        try:
+            # Verifica se a imagem está presente no bucket
+            s3_client.head_object(Bucket=bucket_name, Key=object_name)
+            print(f"O arquivo {object_name} já existe no bucket {bucket_name}")
+            return False
+    
+        except ClientError as e:
+            # Se não existir, a exceção é lançada e o código continua
+            error_code = e.response['Error']['Code']
+    
+            if error_code == '404':
+                # objeto não existe no bucket então faz o upload
+                try: 
+                    s3_client.upload_file(image_name, bucket_name, object_name)
+                    print(f"Arquivo {object_name} enviado com sucesso para o bucket {bucket_name}")
+                    return True    
+                except NoCredentialsError:
+                    print("Credenciais não encontradas")
+                    return False
+            else:
+                # Outros erros
+                print(f"Erro ao tentar o objeto {object_name} no bucket {bucket_name}: {error_code}")
+                return False
 
-criar_classe = S3BucketClass(S3_BUCKET_NAME)
+    # Função para obter os metadados de uma imagem no S3 e retornar o objeto de metadados
+    def get_image_metadata(self, bucket, image_name):
+        metadata = self.s3.head_object(Bucket=bucket, Key=image_name)
+        return metadata
+        
+    # Função para gerar uma URL pública para acessar a imagem no S3
+    def get_signed_url(self, bucket, image_name):
+        url = f'https://{bucket}.s3.amazonaws.com/{image_name}'
+        return url
