185a87908f458ee3ed873d9fa3ffba711cf89c71
rafael-torres-nantes <rafatorresnantes@gmail.com>
2025-01-23 18:14:02 -0400

    refactor: rename request_api.py to send_post_request.py and update function definitions with docstrings in Portuguese

--- Utill Python/send_post_request.py
+++ Utill Python/send_post_request.py
@@ -4,18 +4,31 @@ import requests
 import pandas as pd
 from dotenv import load_dotenv
 
-def load_json_file(json_file_path):
+def load_json_file(json_path):
+    """
+    Função para carregar um arquivo JSON.
+
+    Args:
+        json_path (str): Caminho do arquivo JSON.
+    """
     
-    with open(json_file_path, 'r') as file:
+    with open(json_path, 'r') as file:
         json_data = json.load(file)
 
     return json_data
 
-def main():
+def send_post_request():
+    """
+    Função para enviar um POST request para a API qualquer.
+
+    Returns:
+        dict: Resultado da API.
+    """
+
     # Dados para enviar no POST request
     data = load_json_file('./test_example.json')
 
-    # Enviar o POST request
+    # Enviar o POST request para a API
     response = requests.post("https://fbpdfs3097.execute-api.us-east-1.amazonaws.com/v1/vision", json=data)
 
     # Verificar se a request foi bem-sucedida
@@ -29,4 +42,4 @@ def main():
 
 
 if (__name__ == '__main__'):
-    main()
+    send_post_request()
