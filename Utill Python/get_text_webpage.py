7c76cbee8e6dd6bfaa66e84991b472f7e10e7d96
rafael-torres-nantes <rafatorresnantes@gmail.com>
2025-01-23 18:10:33 -0400

    refactor: update get_text_webpage.py with function docstring and comments in Portuguese

--- Utill Python/get_text_webpage.py
+++ Utill Python/get_text_webpage.py
@@ -1,22 +1,29 @@
 import requests
-from bs4 import BeautifulSoup # beautifulsoup4
-
-# pip install beautifulsoup4
+from bs4 import BeautifulSoup
 
 def extract_text(url):
-    # Fetch the webpage content
+    """
+    Função para extrair texto de uma página da web. 
+
+    Args:
+        url (str): URL da página da web.
+
+    Returns:
+        str: Texto extraído da página da web.
+    """
+    # Faz uma requisição GET para a URL
     response = requests.get(url)
     
-    # Parse the HTML content
+    # Parseia o conteúdo da página com BeautifulSoup
     soup = BeautifulSoup(response.text, 'html.parser')
     
-    # Extract text from all paragraph tags
+    # Extrai o texto de todos os parágrafos da página
     paragraphs = soup.find_all('p')
     text = ' '.join([p.get_text() for p in paragraphs])
     
     return text
 
-# Example usage
+# Exemplo de uso
 url = 'https://stackoverflow.com/questions/18831380/how-can-i-from-bs4-import-beautifulsoup'
 extracted_text = extract_text(url)
 print(extracted_text)
